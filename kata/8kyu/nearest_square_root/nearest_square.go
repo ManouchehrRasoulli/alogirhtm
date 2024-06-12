01ca02782cc221486c67b138ffbaf18986b77977
m.rasouli <m.rasouli@asanpardakht.ir>
2024-06-12 08:41:24 +0330

    - start kata

---
+++ kata/8kyu/nearest_square_root/nearest_square.go
@@ -0,0 +1,26 @@
+package nearest_square_root
+
+import (
+	"math"
+)
+
+func NearestSq(n int) int {
+	// Code goes here
+	square := math.Pow(float64(n), 0.5)
+	squareInt := int(square)
+
+	if int(square*10) == squareInt*10 { // perfect square
+		return squareInt
+	}
+	oneBelow := math.Pow(float64(squareInt), 2)
+	oneAbove := math.Pow(float64(squareInt)+1, 2)
+
+	difBelow := n - int(oneBelow)
+	difAbove := int(oneAbove) - n
+
+	if difBelow < difAbove {
+		return int(oneBelow)
+	}
+
+	return int(oneAbove)
+}
---
+++ kata/8kyu/nearest_square_root/nearest_square_test.go
@@ -0,0 +1,7 @@
+package nearest_square_root
+
+import "testing"
+
+func TestNearestSq(t *testing.T) {
+	t.Log(NearestSq(10))
+}
