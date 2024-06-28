4a486bdb19189edbd9a92ee9f9d873fe09999cc2
m.rasouli <m.rasouli@asanpardakht.ir>
2024-06-28 16:19:08 +0330

    - solve hoop in js

---
+++ kata/8kyu/hoop.js
@@ -0,0 +1,10 @@
+
+function hoopCount (n) {
+    if (n >= 10) {
+        return "Great, now move on to tricks";
+    }
+    return "Keep at it until you get it";
+}
+
+console.log(hoopCount(3));
+console.log(hoopCount(11));
