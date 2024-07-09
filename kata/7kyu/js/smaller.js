7582e0b19ccebf74fb363c1dcd8cf4f47146fa51
m.rasouli <m.rasouli@asanpardakht.ir>
2024-07-09 15:23:55 +0330

    - better solution for smaller

--- kata/7kyu/js/smaller.js
+++ kata/7kyu/js/smaller.js
@@ -19,7 +19,34 @@ function smaller(nums) {
 // smaller_b
 // O(n)
 function smaller_b(nums) {
-    
+    let m = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let v = m.get(nums[i]);
+        if (v === undefined) {
+            m.set(nums[i], 1);
+            continue;
+        }
+
+        m.set(nums[i], m.get(nums[i]) + 1);
+    }
+
+    console.log(m);
+
+    let r = [nums.length];
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums[i]; j++) {
+            let v = m.get(j);
+            if (v !== undefined) {
+                count += v;
+            }
+        }
+        r[i] = count;
+    }
+
+    return r;
 }
 
 console.log(smaller([5, 4, 3, 2, 1]));
+console.log(smaller_b([5, 4, 3, 2, 1]));
