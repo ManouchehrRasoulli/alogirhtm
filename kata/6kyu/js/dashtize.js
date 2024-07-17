5793c40611d47416fd91e3b3bb45933334a8f8d5
m.rasouli <m.rasouli@asanpardakht.ir>
2024-07-17 09:03:58 +0330

    - dashtize

--- kata/6kyu/js/dashtize.js
+++ kata/6kyu/js/dashtize.js
@@ -1,10 +1,15 @@
 function dashatize(num) {
+    if (num < 0) {
+        num = num * -1;
+    }
     let result = "";
     String(num).split('').forEach((value, index, arr) => {
         let n = Number(value);
         if (n % 2 === 1) {
             if (index !== 0 && index !== arr.length - 1) {
                 result = result + "-" + n + "-";
+            } else if (index === 0 && arr.length === 1) {
+                result = n + "";
             } else if (index === 0) {
                 result = n + "-";
             } else {
@@ -12,10 +17,12 @@ function dashatize(num) {
             }
             return;
         }
-        result = result + n;
+        result = result + "" + n;
     });
-    result = result.replaceAll("--", "-");
-    return result;
+    return result.replaceAll("--", "-");
 }
 
+console.log(dashatize(-1));
+console.log(dashatize(222));
+console.log(dashatize(274));
 console.log(dashatize(5311));
