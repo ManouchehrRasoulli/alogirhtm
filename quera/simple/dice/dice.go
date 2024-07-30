3825e43a2ecb266dd3575290c1aa4007bf006c06
m.rasouli <m.rasouli@asanpardakht.ir>
2024-07-30 17:44:54 +0330

    - quera simple question

---
+++ quera/simple/dice/dice.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var x string
+	_, err := fmt.Scanln(&x)
+	if err != nil {
+		log.Println("got error reading input", err)
+		os.Exit(-1)
+	}
+
+	var num int
+	num, err = strconv.Atoi(x)
+	if err != nil {
+		log.Println("non integer input", err, x)
+		os.Exit(-1)
+	}
+
+	switch num {
+	case 1:
+		fmt.Println(6)
+	case 2:
+		fmt.Println(5)
+	case 3:
+		fmt.Println(4)
+	case 4:
+		fmt.Println(3)
+	case 5:
+		fmt.Println(2)
+	default:
+		fmt.Println(1)
+	}
+}
