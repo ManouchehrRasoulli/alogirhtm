7665daa41f583fb5974dfcaab1327c93ed3b506e
m.rasouli <m.rasouli@asanpardakht.ir>
2024-07-31 19:41:50 +0330

    - complete elevator

--- kata/4kyu/elevator/elevator.go
+++ kata/4kyu/elevator/elevator.go
@@ -68,14 +68,14 @@ func (s *Slice[Item]) Pop(index int) (Item, error) {
 type Building struct {
 	MaxFloor int
 	MinFloor int
-	Floor    map[int]*Slice[Person]
+	floors   map[int]*Slice[Person]
 }
 
 func NewBuilding() Building {
 	b := Building{
 		MaxFloor: math.MinInt,
 		MinFloor: math.MaxInt,
-		Floor:    make(map[int]*Slice[Person]),
+		floors:   make(map[int]*Slice[Person]),
 	}
 	return b
 }
@@ -83,16 +83,47 @@ func NewBuilding() Building {
 func (b *Building) Add(p Person) {
 	b.MinFloor = minItem(b.MinFloor, p.From, p.To)
 	b.MaxFloor = maxItem(b.MaxFloor, p.From, p.To)
-	if _, ok := b.Floor[p.From]; !ok {
-		b.Floor[p.From] = &Slice[Person]{
-			items: make([]Person, 0),
-		}
+	if _, ok := b.floors[p.From]; !ok {
+		s := NewSlice[Person]()
+		b.floors[p.From] = &s
 	}
-	b.Floor[p.From].Push(p)
+	b.floors[p.From].Push(p)
 }
 
-func (b *Building) PersonsMovingUp(floor int, count int) []Person {
-	panic("not implemented")
+func (b *Building) AnyPersonLeft() bool {
+	for _, f := range b.floors {
+		if f.Len() > 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (b *Building) PersonsInDirection(floor int, count int, direction bool) []Person {
+	persons := b.floors[floor]
+	if persons == nil {
+		return nil
+	}
+
+	p := make([]Person, 0)
+	for i := 0; i < persons.Len() && len(p) < count; {
+		person, err := persons.At(i)
+		if err != nil {
+			break
+		}
+
+		if (direction == Up && person.From < person.To) ||
+			(direction == Down && person.From > person.To) { // person moving up
+			p = append(p, person)
+			_, _ = persons.Pop(i)
+			continue
+		}
+
+		i++
+	}
+
+	return p
 }
 
 const (
@@ -101,14 +132,122 @@ const (
 )
 
 type Elevator struct {
-	capacity  int
-	members   Slice[Person]
-	building  *Building
-	direction bool
+	currentFloor int
+	capacity     int
+	onboard      int
+	to           Slice[Person]
+	building     Building
+	direction    bool
+}
+
+func NewElevator(currentFloor int, direction bool, capacity int, building Building) Elevator {
+	e := Elevator{
+		currentFloor: currentFloor,
+		capacity:     capacity,
+		onboard:      0,
+		to:           NewSlice[Person](),
+		building:     building,
+		direction:    direction,
+	}
+
+	return e
+}
+
+func (e *Elevator) HowManyCanBoard() int {
+	return e.capacity - e.onboard
+}
+
+func (e *Elevator) ToNextFloor() {
+	if e.direction == Up {
+		e.currentFloor++
+	} else {
+		e.currentFloor--
+	}
+	if e.currentFloor > e.building.MaxFloor {
+		e.direction = Down
+		e.currentFloor = e.building.MaxFloor
+	}
+	if e.currentFloor < e.building.MinFloor {
+		e.direction = Up
+		e.currentFloor = e.building.MinFloor
+	}
+}
+
+func (e *Elevator) AnyOneCouldBoard() bool {
+	persons := e.building.PersonsInDirection(e.currentFloor, e.HowManyCanBoard(), e.direction)
+	for _, p := range persons {
+		e.to.Push(p)
+		e.onboard++
+		fmt.Printf("enter << %+v, floor %d, capacity %d\n", p, e.currentFloor, e.HowManyCanBoard())
+	}
+	return len(persons) != 0
+}
+
+func (e *Elevator) AnyOneCouldExit() bool {
+	x := e.onboard
+	anyOneExited := false
+	index := 0
+
+	for i := 0; i < x; i++ {
+		p, err := e.to.At(index)
+		if err != nil {
+			break
+		}
+
+		if p.To == e.currentFloor {
+			fmt.Printf("exit >> %+v - floor %d\n", p, e.currentFloor)
+			_, _ = e.to.Pop(index)
+			e.onboard--
+			anyOneExited = true
+			continue
+		}
+
+		index++
+	}
+
+	return anyOneExited
+}
+
+func (e *Elevator) NextStop() (int, error) {
+	for e.building.AnyPersonLeft() || e.onboard != 0 {
+		if e.AnyOneCouldBoard() || e.AnyOneCouldExit() {
+			return e.currentFloor, nil
+		}
+
+		e.ToNextFloor()
+	}
+
+	return -1, fmt.Errorf("noone left")
 }
 
 func Order(level int, queue []Person) []int {
-	return []int{}
+	fmt.Printf("levle %d, queue %+v\n", level, queue)
+
+	b := NewBuilding()
+	for _, p := range queue {
+		b.Add(p)
+	}
+	e := NewElevator(level, Up, 5, b)
+
+	stops := make([]int, 0)
+	for {
+		lv, err := e.NextStop()
+		if err != nil {
+			break
+		}
+
+		stops = append(stops, lv)
+	}
+
+	reducedStops := make([]int, 0)
+	for i := 1; i < len(stops); i++ {
+		if stops[i] != stops[i-1] {
+			reducedStops = append(reducedStops, stops[i-1], stops[i])
+			i += 2
+		}
+	}
+
+	return reducedStops
 }
 
 type Person struct {
--- kata/4kyu/elevator/elevator_test.go
+++ kata/4kyu/elevator/elevator_test.go
@@ -27,6 +27,9 @@ func TestSlice(t *testing.T) {
 
 	t.Log(s.Pop(1))
 	t.Log(s)
+
+	t.Log(s.Pop(0))
+	t.Log(s.Pop(0))
 }
 
 func TestBuilding(t *testing.T) {
@@ -42,4 +45,49 @@ func TestBuilding(t *testing.T) {
 	b.Add(p)
 
 	t.Log(b)
+	t.Log(b.PersonsInDirection(1, 3, Up))
+	t.Log(b.AnyPersonLeft())
+
+	t.Log(b.PersonsInDirection(3, 0, Up))
+	t.Log(b.AnyPersonLeft())
+}
+
+func TestElevator(t *testing.T) {
+	b := NewBuilding()
+	p := Person{
+		From: 1,
+		To:   4,
+	}
+	b.Add(p)
+	p.To = 2
+	b.Add(p)
+	p.From = 2
+	p.To = 4
+	b.Add(p)
+
+	e := NewElevator(1, Up, 5, b)
+	t.Log(e.NextStop())
+	t.Log(e.NextStop())
+	t.Log(e.NextStop())
+	t.Log(e.NextStop())
+	t.Log(e.NextStop())
+	t.Log(e.NextStop())
+	t.Log(e.NextStop())
+}
+
+func TestOrder1(t *testing.T) {
+	startingFloor := 1
+	puzzle := []Person{
+		{From: 3, To: 2}, // Al
+		{From: 5, To: 2}, // Betty
+		{From: 2, To: 1}, // Charles
+		{From: 2, To: 5}, // Dan
+		{From: 4, To: 3}, // Ed
+	}
+	solution := []int{2, 5, 4, 3, 2, 1}
+
+	s := Order(startingFloor, puzzle)
+
+	t.Log(s)
+	t.Log(solution)
 }
