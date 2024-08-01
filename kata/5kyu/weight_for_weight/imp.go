adefeb715f2dd543c3caf67d9b724b62445c6ce2
m.rasouli <m.rasouli@asanpardakht.ir>
2024-08-01 07:51:25 +0330

    - weighted sort

---
+++ kata/5kyu/weight_for_weight/imp.go
@@ -0,0 +1,57 @@
+package kata
+
+import (
+	"strconv"
+	"strings"
+)
+
+func sort(param []string, fn func(a, b string) int) {
+	for i := 0; i < len(param); i++ {
+		for j := i + 1; j < len(param); j++ {
+			if fn(param[i], param[j]) > 0 {
+				param[i], param[j] = param[j], param[i]
+			}
+		}
+	}
+}
+
+func OrderWeight(str string) string {
+	sp := strings.Split(str, " ")
+	spf := make([]string, 0)
+	for _, x := range sp {
+		if x == " " {
+			continue
+		}
+
+		spf = append(spf, x)
+	}
+
+	sort(spf, func(a, b string) int {
+		sm := func(x string) int {
+			xSum := 0
+			for _, v := range x {
+				f, _ := strconv.Atoi(string(v))
+				xSum += f
+			}
+
+			return xSum
+		}
+
+		aSum := sm(a)
+		bSum := sm(b)
+
+		if aSum == bSum {
+			if a <= b {
+				return -1
+			}
+			return 1
+		}
+
+		if aSum <= bSum {
+			return -1
+		}
+		return 1
+	})
+
+	return strings.Join(spf, " ")
+}
---
+++ kata/5kyu/weight_for_weight/imp_test.go
@@ -0,0 +1,7 @@
+package kata
+
+import "testing"
+
+func TestOrderWeight(t *testing.T) {
+	t.Log(OrderWeight("103 123 44442 99 2000"))
+}
