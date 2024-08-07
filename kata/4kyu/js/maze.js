991f4f0053796148185d2d92b5d91b8cb15deee8
m.rasouli <m.rasouli@asanpardakht.ir>
2024-08-07 23:16:36 +0330

    - better solution

--- kata/4kyu/js/maze.js
+++ kata/4kyu/js/maze.js
@@ -170,3 +170,31 @@ WWWW.......W.W..W......WW..W..W.W.W..WWW....W..W.W..W....W...W...WWW.....W......
 console.log(pathFinder(m)); // false
 
 // the solution failed for memmory space
+
+
+/* best solution
+
+function pathFinder(maze){
+  const rows = maze.split(`\n`).map(l => l.split(``));
+  const n = rows.length - 1;
+  const moveTo = (x, y) => {
+    if (x < 0 || y < 0 || x > n || y > n || rows[y][x] !== '.') {
+      return false;
+    }
+
+    if (x === n && y === n) {
+      return true;
+    }
+
+    rows[y][x] = `x`;
+
+    return moveTo(x - 1, y)
+      || moveTo(x + 1, y)
+      || moveTo(x, y - 1)
+      || moveTo(x, y + 1);
+  }
+
+  return moveTo(0, 0);
+}
+
+ */
