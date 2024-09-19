3ae8deb3fc6f3f0dbdc859fa76e3ef346d382b63
m.rasouli <m.rasouli@asanpardakht.ir>
2024-09-19 10:50:43 +0330

    - skyscraper

--- kata/4kyu/skyscraper/solution.go
+++ kata/4kyu/skyscraper/solution.go
@@ -1,7 +1,5 @@
 package kata
 
-import "fmt"
-
 type dir int
 
 const (
@@ -37,9 +35,9 @@ var clueLoc = [16]location{
 	{0, 0, right, 15},
 }
 
-func findClueLoc(row, col int, lastIndex int) location {
+func findClueLoc(row, col int, lastIndex int, lastDir dir) location {
 	for _, c := range clueLoc {
-		if c.x == row && c.y == col && c.index > lastIndex {
+		if c.x == row && c.y == col && c.index > lastIndex && c.dir != lastDir {
 			return c
 		}
 	}
@@ -49,10 +47,10 @@ func findClueLoc(row, col int, lastIndex int) location {
 
 // clues helper
 func getClues(row, col int) [4]location {
-	w := findClueLoc(0, col, -1)
-	x := findClueLoc(row, 3, w.index)
-	y := findClueLoc(3, col, x.index)
-	z := findClueLoc(row, 0, y.index)
+	w := findClueLoc(0, col, -1, right)
+	x := findClueLoc(row, 3, w.index, w.dir)
+	y := findClueLoc(3, col, x.index, x.dir)
+	z := findClueLoc(row, 0, y.index, y.dir)
 	locs := [4]location{
 		w,
 		x,
@@ -63,7 +61,7 @@ func getClues(row, col int) [4]location {
 	return locs
 }
 
-func scenes(row, col, d dir, board [4][4]int) int {
+func scenes(row int, col int, d dir, board [4][4]int) int {
 	sceene := 0
 	maxScene := 0
 	switch d {
@@ -104,12 +102,97 @@ func scenes(row, col, d dir, board [4][4]int) int {
 	return sceene
 }
 
-func matchClues(board [4][4]int, clues []int, row, col int) bool {
+func violateSudokuRule(board [4][4]int, row, col int, num int) bool {
+	for i := 0; i < 4; i++ {
+		if board[row][i] == num {
+			return true
+		}
+	}
+
+	for j := 0; j < 4; j++ {
+		if board[j][col] == num {
+			return true
+		}
+	}
+
+	return false
+}
+
+func violateSkyscraperRule(board [4][4]int, clues []int, row, col int) bool {
 	clueLocations := getClues(row, col)
 	for _, c := range clueLocations {
-		fmt.Println(c, clues[c.index])
+		view := scenes(c.x, c.y, c.dir, board)
+		if clues[c.index] != 0 && view != clues[c.index] {
+			return true
+		}
 	}
-	panic("not implemented")
+	return false
+}
+
+func violate(board [4][4]int, clues []int, row, col, num int) bool {
+	if violateSudokuRule(board, row, col, num) {
+		return true
+	}
+
+	if violateSkyscraperRule(board, clues, row, col) {
+		return true
+	}
+
+	return false
+}
+
+func isCompleted(board [4][4]int) (is bool, row, col int) {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if board[i][j] == 0 {
+				return false, i, j
+			}
+		}
+	}
+
+	return true, -1, -1
+}
+
+func solveSudoku(board [4][4]int) (bool, [4][4]int) {
+	cmp, row, col := isCompleted(board)
+	if cmp {
+		return true, board
+	}
+
+	for i := 4; i > 0; i-- {
+		if !violateSudokuRule(board, row, col, i) {
+			board[row][col] = i
+			ok, x := solveSudoku(board)
+			if ok {
+				return ok, x
+			}
+		} else {
+			board[row][col] = 0
+		}
+	}
+
+	return false, board
+}
+
+func solveSkyscraperSudoku(clues []int, board [4][4]int) (bool, [4][4]int) {
+	cmp, row, col := isCompleted(board)
+	if cmp {
+		return true, board
+	}
+
+	for i := 4; i > 0; i-- {
+		if !violate(board, clues, row, col, i) {
+			board[row][col] = i
+			ok, x := solveSudoku(board)
+			if ok {
+				return ok, x
+			}
+		} else {
+			board[row][col] = 0
+		}
+	}
+
+	return false, board
 }
 
 func SolvePuzzle(clues []int) [][]int {
@@ -122,7 +205,21 @@ func SolvePuzzle(clues []int) [][]int {
 		{0, 0, 0, 0},
 	}
 
-	fmt.Println(board[1][2])
+	ok, b := solveSkyscraperSudoku(clues, board)
+	if ok {
+		x := make([][]int, 0)
+		for i := 0; i < 4; i++ {
+			x[i] = make([]int, 0)
+		}
 
-	panic("not implemented yet")
+		for i := 0; i < 4; i++ {
+			for j := 0; j < 4; j++ {
+				x[i] = append(x[i], b[i][j])
+			}
+		}
+
+		return x
+	}
+
+	return nil
 }
--- kata/4kyu/skyscraper/solution_test.go
+++ kata/4kyu/skyscraper/solution_test.go
@@ -2,6 +2,63 @@ package kata
 
 import "testing"
 
+func TestViolateSudokuRule(t *testing.T) {
+	var boar = [4][4]int{
+		{2, 1, 4, 3},
+		{3, 0, 1, 2},
+		{4, 2, 3, 1},
+		{1, 3, 2, 4},
+	}
+
+	t.Log(violateSudokuRule(boar, 1, 1, 3))
+	t.Log(violateSudokuRule(boar, 1, 1, 4))
+}
+
+func TestViolateSkyscraperRule(t *testing.T) {
+	var clues = []int{
+		0, 0, 1, 2,
+		0, 2, 0, 0,
+		0, 3, 0, 0,
+		0, 1, 0, 2}
+
+	var boar = [4][4]int{
+		{2, 1, 4, 3},
+		{3, 4, 1, 2},
+		{4, 2, 3, 1},
+		{1, 3, 2, 4},
+	}
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if violateSkyscraperRule(boar, clues, i, j) {
+				t.Error(i, j)
+			}
+			x := boar[i][j]
+			boar[i][j] = 0
+			if violateSudokuRule(boar, i, j, x) {
+				t.Error(i, j, "sudoko !")
+			}
+			boar[i][j] = x
+		}
+	}
+}
+
+func TestSolveSudoku(t *testing.T) {
+	board := [4][4]int{
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+	}
+
+	ok, b := solveSudoku(board)
+	t.Log(ok)
+
+	for i := 0; i < 4; i++ {
+		t.Log(b[i])
+	}
+}
+
 func TestSceene(t *testing.T) {
 	var boar = [4][4]int{
 		{2, 1, 4, 3},
@@ -19,6 +76,11 @@ func TestSceene(t *testing.T) {
 }
 
 func TestFindClueLocation(t *testing.T) {
-	t.Log(getClues(1, 2))
-	SolvePuzzle([]int{})
+	var clues = []int{
+		0, 0, 1, 2,
+		0, 2, 0, 0,
+		0, 3, 0, 0,
+		0, 1, 0, 2}
+
+	t.Log(SolvePuzzle(clues))
 }
