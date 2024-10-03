02c53f9565fb64d8ae4e3fb3f3a58c0829e3c928
m.rasouli <m.rasouli@asanpardakht.ir>
2024-10-03 18:56:42 +0330

    - start merge with new item

---
+++ leet/56-merge-intervalls-with-new-item/merge.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (intervals, newInterval) {
+};
+
+console.log(insert([[0, 5], [9, 12]], [7, 16]));
+console.log(insert([[0, 2], [3, 3], [6, 11]], [9, 15]));
+console.log(insert([[1, 5]], [0, 0]));
+console.log(insert([[1, 5]], [6, 8]));
+console.log(insert([[1, 5]], [2, 7]));
+console.log(insert([[2, 7]], [1, 5]));
+console.log(insert([[1, 3], [6, 9]], [2, 5]));
+console.log(insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]));
