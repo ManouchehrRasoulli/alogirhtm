0b8fd8c28508e6c75d777d41d059a7566825ee2a
m.rasouli <m.rasouli@asanpardakht.ir>
2024-10-12 17:31:09 +0330

    - 105 optimze

--- leet/105-binraty-tree-preorder-inorder/rec.go
+++ leet/105-binraty-tree-preorder-inorder/rec.go
@@ -18,39 +18,27 @@ func StringTree(root *TreeNode) string {
 	return fmt.Sprintf("{val: %d, left: %s, right: %s}", root.Val, StringTree(root.Left), StringTree(root.Right))
 }
 
-func itemIndex(list []int, item int) int {
-	for i := 0; i < len(list); i++ {
-		if list[i] == item {
-			return i
-		}
-	}
-
-	return -1
-}
-
 func buildTree(preorder []int, inorder []int) *TreeNode {
 	if len(inorder) == 0 || len(preorder) == 0 {
 		return nil
 	}
 
-	val := preorder[0]
 	head := TreeNode{
-		Val:   val,
+		Val:   preorder[0],
 		Left:  nil,
 		Right: nil,
 	}
 
-	inorderIndex := itemIndex(inorder, val)
-	inorderLeft := inorder[:inorderIndex]
-	inorderRight := inorder[1+inorderIndex:]
+	var inorderIndex int
+	for i := 0; i < len(inorder); i++ {
+		if inorder[i] == head.Val {
+			inorderIndex = i
+			break
+		}
+	}
 
-	il := len(inorderLeft) + 1
-
-	preOrderLeft := preorder[1:]
-	preOrderRight := preorder[il:]
-
-	head.Left = buildTree(preOrderLeft, inorderLeft)
-	head.Right = buildTree(preOrderRight, inorderRight)
+	head.Left = buildTree(preorder[1:inorderIndex+1], inorder[:inorderIndex])
+	head.Right = buildTree(preorder[inorderIndex+1:], inorder[1+inorderIndex:])
 
 	return &head
 }
