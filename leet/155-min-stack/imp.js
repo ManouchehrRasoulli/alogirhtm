f55370828ec43eb74d3b6a9e5333fade3c2387af
m.rasouli <m.rasouli@asanpardakht.ir>
2024-10-27 15:21:04 +0330

    - implement better solution for 155

--- leet/155-min-stack/imp.js
+++ leet/155-min-stack/imp.js
@@ -1,4 +1,32 @@
+class BetterMinStack {
+    constructor() {
+        this._array_ = [];
+        this._min_stack_ = [];
+    };
+    push(val) {
+        this._array_.push(val);
+        if (this._min_stack_.length === 0) {
+            this._min_stack_.push(val);
+            return;
+        }
 
+        let current = this._min_stack_[this._min_stack_.length - 1];
+        this._min_stack_.push(Math.min(current, val));
+    };
+
+    pop() {
+        this._min_stack_.pop();
+        return this._array_.pop();
+    };
+
+    top() {
+        return this._array_[this._array_.length - 1];
+    };
+
+    getMin() {
+        return this._min_stack_[this._min_stack_.length - 1];
+    };
+};
 class MinStack {
     constructor() {
         this._array_ = [];
@@ -18,10 +46,9 @@ class MinStack {
     getMin() {
         return Math.min(...this._array_);
     };
-}
+};
 
-
-let minStack = new MinStack();
+let minStack = new BetterMinStack();
 minStack.push(-2);
 minStack.push(0);
 minStack.push(-3);
