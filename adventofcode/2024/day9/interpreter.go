f325085e01dd8ce78676fd8948466e64aa88d541
rasouli <m.rasouli@snappgrocery.com>
2025-03-15 16:08:50 +0330

    algorithm/adventofocode2024: 	* day 9 completed

--- adventofcode/2024/day9/interpreter.go
+++ adventofcode/2024/day9/interpreter.go
@@ -57,6 +57,100 @@ func CompactPartitionPart1(data []int) {
 	}
 }
 
+func CompactPartitionPart2(data []int) {
+	var (
+		blockStart = len(data)
+		blockEnd   = len(data)
+
+		emptyStart = 0
+		emptyEnd   = 0
+	)
+
+	for {
+		blockStart, blockEnd = NextDataBlock(blockStart-1, data)
+		if blockStart == -1 || blockEnd == -1 {
+			break // no file left
+		}
+
+		fileSize := blockEnd + 1 - blockStart
+
+		emptyStart, emptyEnd = NextEmptyBlockWithSize(fileSize, data)
+		if emptyStart == -1 || emptyEnd == -1 {
+			continue
+		}
+
+		if emptyStart > blockEnd {
+			// don't copy
+			continue
+		}
+
+		i := 0
+		for j := emptyStart; j <= emptyEnd && blockStart+i <= blockEnd; j++ {
+			data[j], data[blockStart+i] = data[blockStart+i], data[j]
+			i++
+		}
+	}
+}
+
+func NextEmptyBlockWithSize(size int, data []int) (int, int) {
+	var (
+		start = -1
+		end   = -1
+	)
+
+	for i := 0; i < len(data); i++ {
+		if data[i] == -1 {
+			start = i
+		} else {
+			continue
+		}
+		for j := i; j < len(data); j++ {
+			if data[j] == -1 {
+				end = j
+			} else {
+				break
+			}
+		}
+
+		if end+1-start >= size {
+			return start, end
+		}
+	}
+
+	return -1, -1
+}
+
+func NextDataBlock(from int, data []int) (int, int) {
+	var (
+		start = 0
+		end   = 0
+	)
+
+	if from >= len(data) {
+		from = len(data) - 1
+	}
+
+	for i := from; i >= 0; i-- {
+		if data[i] == -1 {
+			continue
+		}
+
+		end = i
+		for j := i; j >= 0; j-- {
+			if data[j] == data[i] {
+				continue
+			}
+
+			start = j + 1
+			break
+		}
+
+		return start, end
+	}
+
+	return -1, -1
+}
+
 func ChecksumPart1(partition []int) int {
 	checksum := 0
 
--- adventofcode/2024/day9/x_test.go
+++ adventofcode/2024/day9/x_test.go
@@ -2,6 +2,7 @@ package day9
 
 import (
 	"github.com/ManouchehrRasoulli/alogirhtm/adventofcode/2024/helper"
+	"log"
 	"testing"
 )
 
@@ -39,4 +40,49 @@ func TestInputPuzzlePart1(t *testing.T) {
 	CompactPartitionPart1(partition)
 	t.Log(partition)
 	t.Log(ChecksumPart1(partition)) // 6330095022244
+}
+
+func TestNextEmptyBlockWithSize(t *testing.T) {
+	data := []int{0, 0, -1, -1, 12, 12, 12, -1, -1, -1}
+	start, end := NextEmptyBlockWithSize(2, data)
+	t.Log(start, end) // 2, 3
+	start, end = NextEmptyBlockWithSize(3, data)
+	t.Log(start, end) // 7, 9
+	start, end = NextEmptyBlockWithSize(4, data)
+	t.Log(start, end) // -1, -1
+}
+
+func TestNextDataBlock(t *testing.T) {
+	data := []int{0, 0, -1, -1, 12, 12, 12, -1, -1, -1}
+	start, end := NextDataBlock(len(data), data)
+	t.Log(start, end) // 4, 6
+	start, end = NextDataBlock(start-1, data)
+	t.Log(start, end) // 0, 1
+	start, end = NextDataBlock(start-1, data)
+	t.Log(start, end) // -1, -1
+}
+
+func TestInputTestPart2(t *testing.T) {
+	data, err := helper.ReadAll("input_test_part1.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dataRune := ReadInput(data)
+	partition := GeneratePartitionPart1(dataRune)
+	CompactPartitionPart2(partition)
+	log.Println(ChecksumPart1(partition)) // 2858
+}
+
+func TestInputPuzzlePart2(t *testing.T) {
+	data, err := helper.ReadAll("input_puzzle_part1.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dataRune := ReadInput(data)
+
+	partition := GeneratePartitionPart1(dataRune)
+	CompactPartitionPart2(partition)
+	t.Log(ChecksumPart1(partition)) // 6359491814941
 }
