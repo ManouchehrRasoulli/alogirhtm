5143e83c5e72148259019a0549966c7f70f39ee5
rasouli <m.rasouli@snappgrocery.com>
2025-03-16 17:29:17 +0330

    algorithm/adventofcode2024: 	* day 12 part 2 completed

--- adventofcode/2024/day12/garden.go
+++ adventofcode/2024/day12/garden.go
@@ -4,17 +4,18 @@ import (
 	"container/list"
 	"fmt"
 	"github.com/ManouchehrRasoulli/alogirhtm/adventofcode/2024/day4"
+	"log"
 )
 
 const (
-	noValue = '.'
+	noValue = '&'
 )
 
 var (
 	directions = []day4.Direction{
-		day4.Top,
 		day4.Left,
 		day4.Right,
+		day4.Top,
 		day4.Bottom,
 	}
 )
@@ -26,6 +27,17 @@ type Location struct {
 
 func (l *Location) String() string {
 	return fmt.Sprintf("(%d, %d)", l.x, l.y)
+}
+
+type Edge struct {
+	a, b Location
+}
+
+func NormalizeEdge(a, b Location) Edge {
+	if a.x < b.x || (a.x == b.x && a.y < b.y) {
+		return Edge{a, b}
+	}
+	return Edge{b, a}
 }
 
 type Garden struct {
@@ -86,6 +98,87 @@ func (g *Garden) Flood(location Location) (int, int) {
 	return grids, perimeter
 }
 
+func (g *Garden) FencesAmountPart2() int {
+	amount := 0
+
+	for i := 0; i < len(g.Plots); i++ {
+		for j := 0; j < len(g.Plots[i]); j++ {
+			loc := Location{i, j}
+			grid, edges := g.FloodGetEdges(loc)
+			if grid > 0 {
+				log.Println(string(g.Value(loc)), grid, edges)
+			}
+			amount += grid * edges
+		}
+	}
+
+	return amount
+}
+
+func (g *Garden) FloodGetEdges(location Location) (int, int) {
+	var (
+		grids   = 0
+		corners = 0
+		queue   = list.New()
+	)
+
+	queue.PushBack(location)
+	for queue.Len() > 0 {
+		current := queue.Remove(queue.Front()).(Location)
+		if _, visited := g.Visited[current]; visited {
+			continue
+		}
+
+		g.Visited[current] = struct{}{}
+		grids += 1
+		v := g.Value(current)
+
+		ngs := g.Neighbours(current)
+		for _, ng := range ngs {
+			if g.Value(ng) == v {
+				queue.PushBack(ng)
+			}
+		}
+
+		ngs = g.AllNeighbours(current)
+		vtl := g.Value(ngs[0])
+		vt := g.Value(ngs[1])
+		vtr := g.Value(ngs[2])
+		vl := g.Value(ngs[3])
+		vr := g.Value(ngs[4])
+		vbl := g.Value(ngs[5])
+		vb := g.Value(ngs[6])
+		vbr := g.Value(ngs[7])
+
+		if vt != v && vl != v {
+			corners++
+		}
+		if vt == v && vl == v && vtl != v {
+			corners++
+		}
+		if vt != v && vr != v {
+			corners++
+		}
+		if vt == v && vr == v && vtr != v {
+			corners++
+		}
+		if vb != v && vl != v {
+			corners++
+		}
+		if vb == v && vl == v && vbl != v {
+			corners++
+		}
+		if vb != v && vr != v {
+			corners++
+		}
+		if vb == v && vr == v && vbr != v {
+			corners++
+		}
+	}
+
+	return grids, corners // each cornet contribute in two edge line
+}
+
 func (g *Garden) Neighbours(location Location) []Location {
 	lv := g.Value(location)
 	if lv == noValue {
@@ -102,6 +195,20 @@ func (g *Garden) Neighbours(location Location) []Location {
 		if g.Value(ng) == lv {
 			ngs = append(ngs, ng)
 		}
+	}
+
+	return ngs
+}
+
+func (g *Garden) AllNeighbours(location Location) []Location {
+	ngs := make([]Location, 0)
+
+	for _, d := range day4.AlDirections {
+		ng := Location{
+			x: location.x + d.X(),
+			y: location.y + d.Y(),
+		}
+		ngs = append(ngs, ng)
 	}
 
 	return ngs
--- adventofcode/2024/day12/x_test.go
+++ adventofcode/2024/day12/x_test.go
@@ -80,3 +80,86 @@ func TestFencingPuzzle3Part1(t *testing.T) {
 
 	t.Log(garden.FencesAmountPart1()) // 1465968
 }
+
+func TestParseAndFloodTestPart2(t *testing.T) {
+	input, err := helper.ReadAll("input_test1.part2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	garden := ParseGarden(input)
+	t.Log(garden)
+
+	t.Log(garden.FloodGetEdges(Location{
+		x: 1,
+		y: 1,
+	})) // 4, 4
+
+	t.Log(garden.FloodGetEdges(Location{
+		x: 1,
+		y: 0,
+	})) // 17, 12
+}
+
+func TestParseAndFloodTestPart3(t *testing.T) {
+	input, err := helper.ReadAll("input_test3.part1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	garden := ParseGarden(input)
+	t.Log(garden)
+
+	t.Log(garden.FloodGetEdges(Location{
+		x: 0,
+		y: 0,
+	})) // 12, 10
+}
+
+func TestFencingTest1Part2(t *testing.T) {
+	input, err := helper.ReadAll("input_test1.part2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	garden := ParseGarden(input)
+	t.Log(garden)
+
+	t.Log(garden.FencesAmountPart2()) // 236
+}
+
+func TestFencingTest2Part2(t *testing.T) {
+	input, err := helper.ReadAll("input_test2.part2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	garden := ParseGarden(input)
+	t.Log(garden)
+
+	t.Log(garden.FencesAmountPart2()) // 368
+}
+
+func TestFencingTest3Part2(t *testing.T) {
+	input, err := helper.ReadAll("input_test3.part1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	garden := ParseGarden(input)
+	t.Log(garden)
+
+	t.Log(garden.FencesAmountPart2()) // 1206
+}
+
+func TestFencingPuzzlePart2(t *testing.T) {
+	input, err := helper.ReadAll("input_puzzle.part1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	garden := ParseGarden(input)
+	t.Log(garden)
+
+	t.Log(garden.FencesAmountPart2()) // 897702
+}
