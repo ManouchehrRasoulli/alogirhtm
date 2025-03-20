04e1be0480f7bb5687ce45fe7a10f79d09f41e2d
rasouli <m.rasouli@snappgrocery.com>
2025-03-20 17:31:38 +0330

    algorithm/adventofcode2024: 	* day 16 part 2 completed

--- adventofcode/2024/day16/maze.go
+++ adventofcode/2024/day16/maze.go
@@ -3,6 +3,7 @@ package day16
 import (
 	"fmt"
 	"github.com/ManouchehrRasoulli/alogirhtm/adventofcode/2024/helper"
+	"math"
 )
 
 const (
@@ -10,6 +11,7 @@ const (
 	end   = 'E'
 	empty = '.'
 	wall  = '#'
+	path  = 'O'
 )
 
 var (
@@ -37,10 +39,24 @@ func NewDirectionLeftAndRight(d helper.Direction) (left helper.Direction, right 
 	return left, right
 }
 
+type LocationCost struct {
+	location *helper.Location
+	cost     int
+}
+
 type State struct {
-	cost      int // till this point
-	loc       *helper.Location
-	direction helper.Direction
+	cost          int // till this point
+	loc           *helper.Location
+	direction     helper.Direction
+	locationCosts []LocationCost
+}
+
+func (s *State) CaptureLoscationCost(locationCosts ...LocationCost) {
+	if s.locationCosts == nil {
+		s.locationCosts = make([]LocationCost, 0)
+	}
+
+	s.locationCosts = append(s.locationCosts, locationCosts...)
 }
 
 func (s State) Value() int {
@@ -73,9 +89,30 @@ func (m *Maze) Value(loc *helper.Location) rune {
 	return wall
 }
 
-// Solve
+func (m *Maze) PrintPath(location ...LocationCost) {
+	for _, loc := range location {
+		x, y := loc.location.Get()
+		m.grid[x][y] = path
+	}
+}
+
+func (m *Maze) PathCount() int {
+	count := 0
+
+	for _, row := range m.grid {
+		for _, col := range row {
+			if col == path {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+// SolvePart1
 // return the final cost of solving the maze
-func (m *Maze) Solve() int {
+func (m *Maze) SolvePart1() int {
 	var (
 		visited = make(map[Visited]struct{})
 		pq      = helper.NewPriorityQueue()
@@ -136,6 +173,134 @@ func (m *Maze) Solve() int {
 	}
 
 	return -1 // no path found
+}
+
+// SolvePart2
+// return the final cost of solving the maze
+func (m *Maze) SolvePart2() (int, int) {
+	var (
+		visited   = make(map[Visited]struct{})
+		pq        = helper.NewPriorityQueue()
+		minCost   = math.MaxInt
+		allPaths  = make([][]LocationCost, 0)
+		finalPath []LocationCost
+		paths     = 0
+	)
+
+	st := State{
+		cost:      0,
+		loc:       m.start,
+		direction: helper.Right,
+	}
+	pq.Push(st)
+
+	for pq.Len() > 0 {
+		current := (*pq.Pop()).(State)
+		current.CaptureLoscationCost(LocationCost{
+			location: current.loc,
+			cost:     current.cost,
+		})
+
+		if current.loc.Compare(m.end) == 0 {
+			if current.cost < minCost {
+				finalPath = current.locationCosts
+				paths++
+				minCost = current.cost
+			}
+
+			continue
+		}
+
+		vis := Visited{
+			loc:       *current.loc,
+			direction: current.direction,
+		}
+
+		if _, ok := visited[vis]; ok {
+			allPaths = append(allPaths, current.locationCosts)
+			continue
+		}
+
+		visited[vis] = struct{}{}
+
+		newLoc := current.loc.Move(current.direction)
+		if m.Value(newLoc) != wall {
+			nstate := State{
+				cost:      current.cost + 1,
+				loc:       newLoc,
+				direction: current.direction,
+			}
+			nstate.CaptureLoscationCost(current.locationCosts...)
+
+			pq.Push(nstate)
+		}
+
+		left, right := NewDirectionLeftAndRight(current.direction)
+		leftLoc := current.loc.Move(left)
+		rightLoc := current.loc.Move(right)
+
+		if m.Value(leftLoc) != wall {
+			lstate := State{
+				cost:      current.cost + 1001,
+				loc:       leftLoc,
+				direction: left,
+			}
+			lstate.CaptureLoscationCost(current.locationCosts...)
+
+			pq.Push(lstate)
+		}
+
+		if m.Value(rightLoc) != wall {
+			rstate := State{
+				cost:      current.cost + 1001,
+				loc:       rightLoc,
+				direction: right,
+			}
+			rstate.CaptureLoscationCost(current.locationCosts...)
+
+			pq.Push(rstate)
+		}
+	}
+
+	m.PrintPath(finalPath...)
+	m.RecursiveColliedWithEachOther(allPaths, [][]LocationCost{finalPath}, 3)
+
+	return minCost, m.PathCount()
+}
+
+func (m *Maze) RecursiveColliedWithEachOther(paths [][]LocationCost, finalPaths [][]LocationCost, count int) {
+	var (
+		newPaths      = make([][]LocationCost, 0)
+		newFinalPaths = make([][]LocationCost, 0)
+	)
+
+	newFinalPaths = append(newFinalPaths, finalPaths...)
+
+	for _, pth := range paths {
+		for _, finalPath := range finalPaths {
+			if PathsColliedWithEachOther(pth, finalPath) {
+				m.PrintPath(pth...)
+				newFinalPaths = append(newFinalPaths, pth)
+			} else {
+				newPaths = append(newPaths, pth)
+			}
+		}
+	}
+
+	if len(newFinalPaths) > len(finalPaths) && count > 0 {
+		m.RecursiveColliedWithEachOther(newPaths, newFinalPaths, count-1)
+	}
+}
+
+func PathsColliedWithEachOther(path1 []LocationCost, path2 []LocationCost) bool {
+	pth1 := path1[len(path1)-1]
+	for _, pth := range path2 {
+		if pth1.location.Compare(pth.location) == 0 && pth1.cost <= pth.cost {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (m *Maze) String() string {
--- adventofcode/2024/day16/x_test.go
+++ adventofcode/2024/day16/x_test.go
@@ -14,7 +14,7 @@ func TestMazeTest0Part1(t *testing.T) {
 	maze := MazeFromData(data)
 	t.Log(maze)
 
-	t.Log(maze.Solve()) // 2008
+	t.Log(maze.SolvePart1()) // 2008
 }
 
 func TestMazeTest1Part1(t *testing.T) {
@@ -26,7 +26,7 @@ func TestMazeTest1Part1(t *testing.T) {
 	maze := MazeFromData(data)
 	t.Log(maze)
 
-	t.Log(maze.Solve()) // 7036
+	t.Log(maze.SolvePart1()) // 7036
 }
 
 func TestMazeTest2Part1(t *testing.T) {
@@ -38,7 +38,7 @@ func TestMazeTest2Part1(t *testing.T) {
 	maze := MazeFromData(data)
 	t.Log(maze)
 
-	t.Log(maze.Solve()) // 11048
+	t.Log(maze.SolvePart1()) // 11048
 }
 
 func TestMazePuzzlePart1(t *testing.T) {
@@ -50,5 +50,50 @@ func TestMazePuzzlePart1(t *testing.T) {
 	maze := MazeFromData(data)
 	t.Log(maze)
 
-	t.Log(maze.Solve()) // 135536
+	t.Log(maze.SolvePart1()) // 135536
+}
+
+func TestMazeTest1Part2(t *testing.T) {
+	data, err := helper.ReadAll("test1.part1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	maze := MazeFromData(data)
+	t.Log(maze)
+
+	t.Log(maze.SolvePart2()) // 7036
+	t.Log(maze.PathCount())  // 45
+
+	t.Log(maze)
+}
+
+func TestMazeTest2Part2(t *testing.T) {
+	data, err := helper.ReadAll("test2.part1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	maze := MazeFromData(data)
+	t.Log(maze)
+
+	t.Log(maze.SolvePart2()) // 11048
+	t.Log(maze.PathCount())  // 64
+
+	t.Log(maze)
+}
+
+func TestMazePuzzlePart2(t *testing.T) {
+	data, err := helper.ReadAll("puzzle.part1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	maze := MazeFromData(data)
+	t.Log(maze)
+
+	t.Log(maze.SolvePart2()) // 135536
+	t.Log(maze.PathCount())  // 583
+
+	t.Log(maze)
 }
