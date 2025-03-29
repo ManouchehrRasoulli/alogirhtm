9188ea286c79d6ee0ff8d244b0f78453131d4c06
rasouli <m.rasouli@snappgrocery.com>
2025-03-29 23:32:41 +0330

    algorithm/adventofcode2024: 	* day 24 part 1 completed

---
+++ adventofcode/2024/day24/wire.go
@@ -0,0 +1,105 @@
+package day24
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const (
+	and = "AND"
+	or  = "OR"
+	xor = "XOR"
+)
+
+var (
+	wirePattern        = regexp.MustCompile(`([a-z0-9]{3}): ([0-1])`)
+	instructionPattern = regexp.MustCompile(`([a-z0-9]{3})\s+(AND|OR|XOR)\s+([a-z0-9]{3})\s+->\s+([a-z0-9]{3})`)
+)
+
+func DoOperations(lines []string) int {
+	var (
+		wires      = make(map[string]int)
+		operations = make([]string, 0)
+		wireParts  = true
+		zWires     = make([]string, 0)
+		zValues    = make([]string, 0)
+	)
+
+	for _, line := range lines {
+		if wireParts {
+			m := wirePattern.FindAllStringSubmatch(line, -1)
+			if len(m) > 0 {
+				wire := m[0][1]
+				if m[0][2] == "1" {
+					wires[wire] = 1
+				} else {
+					wires[wire] = 0
+				}
+				continue
+			} else {
+				wireParts = false
+				continue
+			}
+		}
+
+		operations = append(operations, line)
+	}
+
+	for len(operations) > 0 {
+		operation := operations[0]
+		m := instructionPattern.FindAllStringSubmatch(operation, -1)
+		if len(m) == 0 {
+			log.Fatalf("invalid operation %q", operation)
+		}
+
+		ia := m[0][1]
+		op := m[0][2]
+		ib := m[0][3]
+		o := m[0][4]
+
+		aw, af := wires[ia]
+		bw, bf := wires[ib]
+
+		if !af || !bf { // inputs didn't have a valid value yet !
+			operations = operations[1:]
+			operations = append(operations, operation)
+			continue
+		}
+
+		switch op {
+		case and:
+			wires[o] = aw & bw
+		case or:
+			wires[o] = aw | bw
+		case xor:
+			wires[o] = aw ^ bw
+		}
+
+		// operation completed
+		operations = operations[1:]
+	}
+
+	for k := range wires {
+		if strings.HasPrefix(k, "z") {
+			zWires = append(zWires, k)
+		}
+	}
+
+	slices.Sort(zWires)
+	slices.Reverse(zWires)
+	for _, v := range zWires {
+		zValues = append(zValues, fmt.Sprintf("%d", wires[v]))
+	}
+
+	zString := strings.Join(zValues, "")
+	zInt, err := strconv.ParseInt(zString, 2, 64)
+	if err != nil {
+		log.Fatal(err, zString)
+	}
+
+	return int(zInt)
+}
---
+++ adventofcode/2024/day24/wire_test.go
@@ -0,0 +1,37 @@
+package day24
+
+import (
+	"github.com/ManouchehrRasoulli/alogirhtm/adventofcode/2024/helper"
+	"strings"
+	"testing"
+)
+
+func TestInputSimplePart1(t *testing.T) {
+	data, err := helper.ReadAll("simple.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+	t.Log(DoOperations(lines)) // 4
+}
+
+func TestInputTestPart1(t *testing.T) {
+	data, err := helper.ReadAll("test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+	t.Log(DoOperations(lines)) // 2024
+}
+
+func TestInputInputPart1(t *testing.T) {
+	data, err := helper.ReadAll("input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+	t.Log(DoOperations(lines)) // 59619940979346
+}
