82f239f4280a2bee6af77f6f826f6b150d163696
rasouli <m.rasouli@snappgrocery.com>
2025-05-31 11:21:41 +0330

    algorithm/adventofcode2023: 	* pass part 1 with naive implementation

--- adventofcode/2023/day9/mirage.go
+++ adventofcode/2023/day9/mirage.go
@@ -1,1 +1,75 @@
 package day9
+
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+func ReadData(input string) (data [][]int) {
+	var (
+		lines = strings.Split(input, "\n")
+	)
+
+	for _, line := range lines {
+		lineItemStr := strings.Split(line, " ")
+		lineItemInt := make([]int, 0)
+		for _, i := range lineItemStr {
+			itemInt, err := strconv.Atoi(i)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			lineItemInt = append(lineItemInt, itemInt)
+		}
+
+		data = append(data, lineItemInt)
+	}
+
+	return data
+}
+
+func findNextNaivePart1(data []int) []int {
+	var (
+		isAllZero = func(data []int) bool {
+			for _, i := range data {
+				if i != 0 {
+					return false
+				}
+			}
+
+			return true
+		}
+
+		generateNextData = func(data []int) []int {
+			output := make([]int, 0)
+			for i := 1; i < len(data); i++ {
+				output = append(output, data[i]-data[i-1])
+			}
+
+			return output
+		}
+	)
+
+	if isAllZero(data) { // last item is zero appended
+		data = append(data, 0)
+		return data
+	}
+
+	nextDataOutput := findNextNaivePart1(generateNextData(data))
+	data = append(data, data[len(data)-1]+nextDataOutput[len(data)-1])
+	return data
+}
+
+func Part1(items [][]int) int {
+	var (
+		sum = 0
+	)
+
+	for _, item := range items {
+		data := findNextNaivePart1(item)
+		sum += data[len(data)-1]
+	}
+
+	return sum
+}
--- adventofcode/2023/day9/mirage_test.go
+++ adventofcode/2023/day9/mirage_test.go
@@ -1,1 +1,30 @@
 package day9
+
+import (
+	"github.com/ManouchehrRasoulli/alogirhtm/adventofcode/2023/helper"
+	"testing"
+)
+
+func TestInputTest1Part1(t *testing.T) {
+	data, err := helper.ReadAll("input.test1.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	items := ReadData(data)
+	t.Log(items)
+
+	t.Log(Part1(items))
+}
+
+func TestInputPuzzlePart1(t *testing.T) {
+	data, err := helper.ReadAll("input.puzzle.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	items := ReadData(data)
+	t.Log(items)
+
+	t.Log(Part1(items))
+}
