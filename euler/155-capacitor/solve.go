1e0600783ef2dc3b365bf48feb7b688d7ef2b904
rasouli <m.rasouli@snappgrocery.com>
2025-06-10 10:31:07 +0330

    algorithm/euler: 	* fix issue

--- euler/155-capacitor/solve.go
+++ euler/155-capacitor/solve.go
@@ -1,49 +1,61 @@
 package euler
 
+type combinationType int
+
+const (
+	combinationTypeParallel combinationType = iota
+	combinationTypeSeries
+)
+
 type Combination struct {
 	Series    int
 	Parallels int
+	Type      combinationType
 }
 
 func (c Combination) Key() int {
-	if c.Series == 0 {
-		return c.Parallels * 60
-	}
+	var (
+		parallelsCapacity int
+		seriesCapacity    int
+	)
 
-	if c.Parallels == 0 {
-		seriesDivisor := float64(1) / 60
-		seriesBase := seriesDivisor * float64(c.Series)
-		if seriesBase == 0 {
-			return 0
-		}
+	parallelsCapacity = c.Parallels * 60
 
-		return int(1 / seriesBase)
-	}
-
-	parallelsBase := 1 / float64(c.Parallels*60)
 	seriesDivisor := float64(1) / 60
 	seriesBase := seriesDivisor * float64(c.Series)
+	if seriesBase != 0 {
+		seriesCapacity = int(1 / seriesBase)
+	}
 
-	return int(1 / (parallelsBase + seriesBase))
+	switch c.Type {
+	case combinationTypeParallel:
+		return seriesCapacity + parallelsCapacity
+	case combinationTypeSeries:
+		var (
+			parallelsBase float64
+			totalBase     float64
+		)
+		if parallelsCapacity != 0 {
+			parallelsBase = 1 / float64(parallelsCapacity)
+		}
+
+		totalBase = seriesBase + parallelsBase
+		if totalBase == 0 {
+			return 0
+		}
+		return int(1 / totalBase)
+	}
+
+	panic("invalid combination type !!")
 }
-
-const (
-	parallel = iota
-	series
-)
 
 var (
 	combinations = make(map[int]struct{})
 )
 
-func countCombinations(capacitors, left int, directions int) {
+func countCombinations(capacitors, left int, directions combinationType) {
 	if left == 0 {
 		return
-	}
-
-	switch directions {
-	case parallel:
-	case series:
 	}
 }
 
--- euler/155-capacitor/solve_test.go
+++ euler/155-capacitor/solve_test.go
@@ -1,35 +1,42 @@
 package euler
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCombination(t *testing.T) {
 	c := Combination{
 		Series:    1,
 		Parallels: 2,
+		Type:      combinationTypeSeries,
 	}
 	t.Log(c.Key()) // 40
 
 	c = Combination{
 		Series:    2,
 		Parallels: 1,
+		Type:      combinationTypeParallel,
 	}
 	t.Log(c.Key()) // 90
 
 	c = Combination{
 		Series:    0,
 		Parallels: 3,
+		Type:      combinationTypeSeries,
 	}
 	t.Log(c.Key()) // 180
 
 	c = Combination{
 		Series:    2,
 		Parallels: 0,
+		Type:      combinationTypeSeries,
 	}
 	t.Log(c.Key()) // 30
 
 	c = Combination{
 		Series:    3,
 		Parallels: 0,
+		Type:      combinationTypeSeries,
 	}
 	t.Log(c.Key()) // 20
 }
