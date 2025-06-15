30e234155bf94bd9286991e1f53d2cab7e448f04
rasouli <m.rasouli@snappgrocery.com>
2025-06-15 13:29:40 +0330

    algorithm/kata: 	* 5kyu base 9 conversion

--- kata/5kyu/faulty_odometer/imp.go
+++ kata/5kyu/faulty_odometer/imp.go
@@ -23,3 +23,61 @@ func FaultyOdometerNaive(n int) int {
 
 	return count
 }
+
+func FaultyOdometerNaiveSecond(n int) int {
+	var (
+		count        = 0
+		containsFour = func(i int) int {
+			iStr := strconv.Itoa(i)
+			v := strings.Index(iStr, "4")
+			if v < 0 {
+				return v
+			}
+
+			return len(iStr) - v - 1
+		}
+	)
+
+	for i := 1; i <= n; {
+		if index := containsFour(i); index >= 0 {
+			next := i + index*10
+			if next == i {
+				next += 1
+			}
+			i = next
+			continue
+		}
+
+		i++
+		count++
+	}
+
+	return count
+}
+
+func FaultyOdometer(n int) int {
+	digitMap := map[rune]rune{
+		'0': '0', '1': '1', '2': '2', '3': '3',
+		'5': '4', '6': '5', '7': '6', '8': '7', '9': '8',
+	}
+
+	nStr := strconv.Itoa(n)
+	base9Str := ""
+
+	for _, ch := range nStr {
+		mapped, ok := digitMap[ch]
+		if !ok {
+			// skip invalid digits like '4'
+			continue
+		}
+		base9Str += string(mapped)
+	}
+
+	// Parse base-9 number as base-10 integer
+	result, err := strconv.ParseInt(base9Str, 9, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return int(result)
+}
--- kata/5kyu/faulty_odometer/imp_test.go
+++ kata/5kyu/faulty_odometer/imp_test.go
@@ -5,6 +5,23 @@ import "testing"
 func TestFaultyOdometerNaive(t *testing.T) {
 	t.Log(FaultyOdometerNaive(13))        // 12
 	t.Log(FaultyOdometerNaive(15))        // 13
+	t.Log(FaultyOdometerNaive(55))        // 40
 	t.Log(FaultyOdometerNaive(2005))      // 1462
 	t.Log(FaultyOdometerNaive(165826622)) // 69517865
+}
+
+func TestFaultyOdometerNaiveSecond(t *testing.T) {
+	t.Log(FaultyOdometerNaiveSecond(13))        // 12
+	t.Log(FaultyOdometerNaiveSecond(15))        // 13
+	t.Log(FaultyOdometerNaiveSecond(55))        // 40
+	t.Log(FaultyOdometerNaiveSecond(2005))      // 1462
+	t.Log(FaultyOdometerNaiveSecond(165826622)) // 69517865
+}
+
+func TestFaultyOdometer(t *testing.T) {
+	t.Log(FaultyOdometer(13))        // 12
+	t.Log(FaultyOdometer(15))        // 13
+	t.Log(FaultyOdometer(55))        // 40
+	t.Log(FaultyOdometer(2005))      // 1462
+	t.Log(FaultyOdometer(165826622)) // 69517865
 }
