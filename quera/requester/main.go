b9383d03a848f8aed94034878286fd4fa76db649
rasouli <m.rasouli@snappgrocery.com>
2025-09-22 12:02:43 +0330

    chore/stage/request: 	* ok

--- quera/requester/main.go
+++ quera/requester/main.go
@@ -12,7 +12,7 @@ func main() {
 
 	numcpu := runtime.NumCPU()
 
-	url := "https://salar-backoffice.stage-express.ir/cs/backoffice/order"
+	url := "https://local-api.stage-express.ir/salar-backoffice/order"
 	method := "GET"
 
 	client := &http.Client{}
@@ -22,7 +22,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiZXlKMWMyVnlTV1FpT2pFd01ESTVMQ0oxYzJWeWJtRnRaU0k2SW0wdWNtRnpiM1ZzYVNKOSIsImV4cCI6MTc1NTUyMzQ2NX0.F8b6D25DS-N1NbszdT597gl61EdzlEFjpGikfrGkFtbW3ZS1a7qyzDsHWcDumtLjGmdSbljTjUi9BN48Fs-YCg")
+	req.Header.Add("Authorization", "Bearer eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiZXlKMWMyVnlTV1FpT2pFd01ESTVMQ0oxYzJWeWJtRnRaU0k2SW0wdWNtRnpiM1ZzYVNKOSIsImV4cCI6MTc1ODUzMTUxNn0.YXpWB-Ds-KTd5gMcmOOmLyncxhtO4QMPhNwAD8ShyymFb_snU2fLMmne7ID6UHQ9rLT5C3-BubrRgAXkX8U6BA")
 
 	ctx, cf := context.WithCancel(context.Background())
 	for i := 0; i < numcpu; i++ {
@@ -38,17 +38,18 @@ func main() {
 							fmt.Println(i, err)
 							return
 						}
-						if res.StatusCode != http.StatusOK {
-							fmt.Println(i, "not ok")
-							continue
-						}
-
 						defer res.Body.Close()
 						body, err := io.ReadAll(res.Body)
 						if err != nil {
-							fmt.Println(err)
-							return
+							fmt.Println(i, err)
+							continue
 						}
+
+						if res.StatusCode != http.StatusOK {
+							fmt.Println(i, "not ok", string(body))
+							continue
+						}
+
 						fmt.Println(string(body))
 						cf()
 						return
