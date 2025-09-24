18dface06c5d878c7fac8bdb10eab566d6d0ed51
rasouli <m.rasouli@snappgrocery.com>
2025-09-24 09:57:58 +0330

    algorithm/adventofcode: 	* day 11 improve part 1

--- adventofcode/2023/ady11/part1.go
+++ adventofcode/2023/ady11/part1.go
@@ -6,72 +6,38 @@ import (
 
 func Part1(universe [][]rune) int {
 	var (
-		galaxies = make(map[*helper.Location]struct{})
+		total    = 0
+		galaxies = make([]*helper.Location, 0)
 	)
 
 	for i, row := range universe {
 		for j, col := range row {
 			if col == galaxy {
-				galaxies[helper.NewLocation(i, j)] = struct{}{}
+				galaxies = append(galaxies, helper.NewLocation(i, j))
 			}
 		}
 	}
 
-	var (
-		total = 0
-		sc    = make(map[helper.Location]struct{})
-	)
-	for glx := range galaxies {
-		distance := bfs(universe, glx)
-		for k, d := range distance {
-			if _, ok := sc[k]; !ok {
-				total += d
-			}
+	for i := 0; i < len(galaxies); i++ {
+		for j := i + 1; j < len(galaxies); j++ {
+			total += manhattanDistance(galaxies[i], galaxies[j])
 		}
-		sc[*glx] = struct{}{}
 	}
 
 	return total
 }
 
-func bfs(universe [][]rune, start *helper.Location) map[helper.Location]int {
+func manhattanDistance(a, b *helper.Location) int {
+	ax, ay := a.Get()
+	bx, by := b.Get()
 
-	distance := make(map[helper.Location]int)
-	visited := make(map[helper.Location]struct{})
-	queue := []QueueItem{{loc: start, distance: 0}}
-
-	for len(queue) > 0 {
-		// Pop front
-		item := queue[0]
-		queue = queue[1:]
-
-		x, y := item.loc.Get()
-		if x < 0 || y < 0 || x >= len(universe) || y >= len(universe[0]) {
-			continue
-		}
-		if _, ok := visited[*item.loc]; ok {
-			continue
-		}
-		visited[*item.loc] = struct{}{}
-
-		if universe[x][y] == galaxy {
-			if _, ok := distance[*item.loc]; !ok {
-				distance[*item.loc] = item.distance
-			}
-		}
-
-		for _, dir := range helper.PathDirections {
-			next := item.loc.Move(dir)
-			if _, seen := visited[*next]; !seen {
-				queue = append(queue, QueueItem{loc: next, distance: item.distance + 1})
-			}
-		}
+	dx := ax - bx
+	if dx < 0 {
+		dx = -dx
 	}
-
-	return distance
-}
-
-type QueueItem struct {
-	loc      *helper.Location
-	distance int
+	dy := ay - by
+	if dy < 0 {
+		dy = -dy
+	}
+	return dx + dy
 }
