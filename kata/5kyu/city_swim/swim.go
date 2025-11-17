24bfb863b3f430a5d825dfd4124c1847c33a5e33
rasouli <m.rasouli@snappgrocery.com>
2025-11-17 17:30:40 +0330

    chore/kata/5kyu: 	* towers

--- kata/5kyu/city_swim/swim.go
+++ kata/5kyu/city_swim/swim.go
@@ -1,66 +1,35 @@
 package kata
 
-import "fmt"
-
-const (
-	flat = 0
-	up   = 1
-	down = -1
-)
-
 func RainVolume(towers []int) int {
-	fmt.Println(towers)
-
 	if len(towers) < 2 {
 		return 0
 	}
 
 	var (
-		direction         = flat
-		peaks             = make([]int, 0)
-		maxValueIndexFunc = func(i, j int) int {
-			if towers[i] > towers[j] {
-				return i
-			}
-			return j
-		}
-		difToDirection = func(i, j int) int {
-			if towers[i] > towers[j] {
-				return down
-			}
-			if towers[i] < towers[j] {
-				return up
-			}
-			return flat
-		}
+		left     = 0
+		right    = len(towers) - 1
+		maxLeft  = 0
+		maxRight = 0
+		water    = 0
 	)
 
-	direction = difToDirection(0, 1)
-	if direction == down {
-		peaks = append(peaks, 0)
-	}
-
-	for i := 2; i < len(towers); i++ {
-		currentDirection := difToDirection(i-1, i)
-		if currentDirection == direction {
-			continue
-		}
-
-		if direction == up && currentDirection == down {
-			peaks = append(peaks, maxValueIndexFunc(i-1, i))
-			direction = currentDirection
-		}
-
-		if direction == down && currentDirection == up {
-			direction = currentDirection
+	for left < right {
+		if towers[left] < towers[right] {
+			if towers[left] >= maxLeft {
+				maxLeft = towers[left]
+			} else {
+				water += maxLeft - towers[left]
+			}
+			left++
+		} else {
+			if towers[right] >= maxRight {
+				maxRight = towers[right]
+			} else {
+				water += maxRight - towers[right]
+			}
+			right--
 		}
 	}
 
-	if direction == up {
-		peaks = append(peaks, len(towers)-1)
-	}
-
-	fmt.Println(peaks)
-
-	return 0
+	return water
 }
--- kata/5kyu/city_swim/swim_test.go
+++ kata/5kyu/city_swim/swim_test.go
@@ -3,7 +3,7 @@ package kata
 import "testing"
 
 func TestRainVolume(t *testing.T) {
-	t.Log(RainVolume([]int{5, 2, 4}))                // 3
+	t.Log(RainVolume([]int{5, 2, 6}))                // 3
 	t.Log(RainVolume([]int{1, 0, 5, 2, 6, 3, 10}))   // 7
 	t.Log(RainVolume([]int{15, 0, 6, 10, 11, 2, 5})) // 20
 	t.Log(RainVolume([]int{1, 5, 1}))                // 0
