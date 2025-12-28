33f73bbef33e8fc40ed28982892fe23277a0d25a
rasouli <manouchehrrasouli96@gmail.com>
2025-12-28 18:43:55 +0330

    chore(adventofcode): 2025 day 9 part 2

--- adventofcode/2025/day9/part2.go
+++ adventofcode/2025/day9/part2.go
@@ -7,137 +7,149 @@ import (
 
 func MaxRectangleInPolygon(input string) int {
 	var (
-		lines          = strings.Split(strings.TrimSpace(input), "\n")
-		points         [][2]int
-		maxRow, maxCol int
-		minRow, minCol int = int(^uint(0) >> 1), int(^uint(0) >> 1)
-		grid           [][]rune
-		redTiles       [][2]int
-	)
-
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-		var col, row int
-		_, err := fmt.Sscanf(line, "%d,%d", &col, &row)
-		if err != nil {
-			panic(fmt.Sprintf("failed to parse line %q: %v", line, err))
-		}
-
-		maxRow = max(maxRow, row)
-		minRow = min(minRow, row)
-		maxCol = max(maxCol, col)
-		minCol = min(minCol, col)
-
-		points = append(points, [2]int{row, col})
-	}
-
-	height := maxRow - minRow + 1
-	width := maxCol - minCol + 1
-
-	grid = make([][]rune, height)
-	for i := 0; i < height; i++ {
-		grid[i] = make([]rune, width)
-		for j := 0; j < width; j++ {
-			grid[i][j] = '.'
-		}
-	}
-
-	for _, point := range points {
-		r := point[0] - minRow
-		c := point[1] - minCol
-		grid[r][c] = '#'
-		redTiles = append(redTiles, [2]int{r, c})
-	}
-
-	drawLine := func(from, to [2]int) {
-		r1, c1 := from[0]-minRow, from[1]-minCol
-		r2, c2 := to[0]-minRow, to[1]-minCol
-
-		if r1 == r2 {
-			if c1 > c2 {
-				c1, c2 = c2, c1
+		lines              = strings.Split(strings.TrimSpace(input), "\n")
+		points             [][2]int
+		polygon            [][2][2]int
+		pointOnSegmentFunc = func(p, a, b [2]int) bool {
+			px, py := p[0], p[1]
+			ax, ay := a[0], a[1]
+			bx, by := b[0], b[1]
+			cross := (px-ax)*(by-ay) - (py-ay)*(bx-ax)
+			if cross != 0 {
+				return false
 			}
-			for c := c1; c <= c2; c++ {
-				if grid[r1][c] == '.' {
-					grid[r1][c] = 'X'
+			minX, maxX := ax, bx
+			minY, maxY := ay, by
+			if bx < ax {
+				minX, maxX = bx, ax
+			}
+			if by < ay {
+				minY, maxY = by, ay
+			}
+			return px >= minX && px <= maxX && py >= minY && py <= maxY
+		}
+		rayCrossSegmentFunc = func(p, a, b [2]int) bool {
+			px, py := float64(p[0]), float64(p[1])
+			ax, ay := float64(a[0]), float64(a[1])
+			bx, by := float64(b[0]), float64(b[1])
+			if ay > by {
+				ax, bx = bx, ax
+				ay, by = by, ay
+			}
+			if ay == by {
+				return false
+			}
+			if py <= ay || py > by {
+				return false
+			}
+			intersectX := ax + (bx-ax)*(py-ay)/(by-ay)
+			return px < intersectX
+		}
+		pointIsInPolygonFunc = func(p [2]int) bool {
+			for _, edge := range polygon {
+				if pointOnSegmentFunc(p, edge[0], edge[1]) {
+					return true
 				}
 			}
-		} else if c1 == c2 {
-			if r1 > r2 {
-				r1, r2 = r2, r1
-			}
-			for r := r1; r <= r2; r++ {
-				if grid[r][c1] == '.' {
-					grid[r][c1] = 'X'
+			count := 0
+			for _, edge := range polygon {
+				if rayCrossSegmentFunc(p, edge[0], edge[1]) {
+					count++
 				}
 			}
+			return count%2 == 1
 		}
-	}
-
-	for i := 1; i < len(points); i++ {
-		drawLine(points[i-1], points[i])
-	}
-	drawLine(points[len(points)-1], points[0])
-
-	// Scanline fill
-	for r := 0; r < height; r++ {
-		inside := false
-		for c := 0; c < width; c++ {
-			if grid[r][c] == '#' || grid[r][c] == 'X' {
-				// Check if next cell is empty (boundary edge)
-				if c+1 < width && grid[r][c+1] == '.' {
-					inside = !inside
-				} else if c+1 >= width {
-					inside = !inside
-				}
-			} else if inside {
-				grid[r][c] = 'X'
-			}
-		}
-	}
-
-	isValidRectangle := func(r1, c1, r2, c2 int) bool {
-		minR, maxR := min(r1, r2), max(r1, r2)
-		minC, maxC := min(c1, c2), max(c1, c2)
-
-		for r := minR; r <= maxR; r++ {
-			for c := minC; c <= maxC; c++ {
-				if grid[r][c] != '#' && grid[r][c] != 'X' {
+		pointsAreInPolygonFunc = func(points [][2]int) bool {
+			for _, point := range points {
+				if !pointIsInPolygonFunc(point) {
 					return false
 				}
 			}
+			return true
 		}
-		return true
-	}
-
-	maxArea := 0
-	for i := 0; i < len(redTiles); i++ {
-		for j := i + 1; j < len(redTiles); j++ {
-			r1, c1 := redTiles[i][0], redTiles[i][1]
-			r2, c2 := redTiles[j][0], redTiles[j][1]
-
-			if r1 != r2 && c1 != c2 {
-				if isValidRectangle(r1, c1, r2, c2) {
-					height := abs(r2-r1) + 1
-					width := abs(c2-c1) + 1
-					area := height * width
-
-					if area > maxArea {
-						maxArea = area
-					}
+		rectangleFromDiagonalFunc = func(p1, p2 [2]int) [][2]int {
+			x1, y1 := p1[0], p1[1]
+			x2, y2 := p2[0], p2[1]
+			return [][2]int{{x1, y1}, {x1, y2}, {x2, y2}, {x2, y1}}
+		}
+		abs = func(a int) int {
+			if a < 0 {
+				return -a
+			}
+			return a
+		}
+		linePointsFunc = func(p1, p2 [2]int) [][2]int {
+			x1, y1 := p1[0], p1[1]
+			x2, y2 := p2[0], p2[1]
+			linePoints := make([][2]int, 0)
+			dx := abs(x2 - x1)
+			dy := abs(y2 - y1)
+			sx := 1
+			if x1 > x2 {
+				sx = -1
+			}
+			sy := 1
+			if y1 > y2 {
+				sy = -1
+			}
+			err := dx - dy
+			for {
+				linePoints = append(linePoints, [2]int{x1, y1})
+				if x1 == x2 && y1 == y2 {
+					break
+				}
+				e2 := 2 * err
+				if e2 > -dy {
+					err -= dy
+					x1 += sx
+				}
+				if e2 < dx {
+					err += dx
+					y1 += sy
 				}
 			}
+			return linePoints
+		}
+		rectangleAreaFunc = func(p1, p2 [2]int) int {
+			width := abs(p2[0]-p1[0]) + 1
+			height := abs(p2[1]-p1[1]) + 1
+			return width * height
+		}
+		maxSizeInsidePolygon = 0
+	)
+
+	for _, line := range lines {
+		x, y := 0, 0
+		_, err := fmt.Sscanf(line, "%d,%d", &x, &y)
+		if err != nil {
+			panic(err)
+		}
+		points = append(points, [2]int{x, y})
+	}
+
+	for i := 1; i < len(points); i++ {
+		polygon = append(polygon, [2][2]int{points[i-1], points[i]})
+	}
+	polygon = append(polygon, [2][2]int{points[len(points)-1], points[0]})
+
+	for x := 0; x < len(points); x++ {
+	bLoop:
+		for y := x + 1; y < len(points); y++ {
+			a, b := points[x], points[y]
+			rectanglePoints := rectangleFromDiagonalFunc(a, b)
+			for i := 1; i < len(rectanglePoints); i++ {
+				linePoints := linePointsFunc(rectanglePoints[i], rectanglePoints[i-1])
+				if !pointsAreInPolygonFunc(linePoints) {
+					continue bLoop
+				}
+			}
+			linePoints := linePointsFunc(rectanglePoints[len(rectanglePoints)-1], rectanglePoints[0])
+			if !pointsAreInPolygonFunc(linePoints) {
+				continue bLoop
+			}
+			maxSizeInsidePolygon = max(maxSizeInsidePolygon, rectangleAreaFunc(a, b))
 		}
 	}
 
-	return maxArea
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+	return maxSizeInsidePolygon
 }
--- adventofcode/2025/day9/part2_test.go
+++ adventofcode/2025/day9/part2_test.go
@@ -7,5 +7,5 @@ func TestMaxRectangleInPolygonSample1(t *testing.T) {
 }
 
 func TestMaxRectangleInPolygonPuzzle(t *testing.T) {
-	t.Log(MaxRectangleInPolygon(puzzle)) //
+	t.Log(MaxRectangleInPolygon(puzzle)) // 223975695 -- wrong
 }
